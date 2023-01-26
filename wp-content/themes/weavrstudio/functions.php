abb524a943e259abdc323e48aec41feacc977054
Eric <minkiericlee@gmail.com>
2023-01-26 11:35:04 -0800

    elementor

--- wp-content/themes/weavrstudio/functions.php
+++ wp-content/themes/weavrstudio/functions.php
@@ -1,15 +1,29 @@
 <?php
 
+
+function register_my_navmenu()
+{
+    register_nav_menus(array(
+        'header' => __('Header', 'headernav'),
+        'footer' => __('Footer', 'footernav'),
+        '404' => __('404', '404nav')
+    ));
+}
+
+add_action('init', 'register_my_navmenu', 1);
+
 if (!function_exists('weavrstudio_setup')) {
     function weavrstudio_setup()
     {
         add_theme_support('post-thumbnails');
-        register_nav_menus(array(
-            'header' => "Header",
-            'footer' => "Footer",
-            '404' => "404",
-        ));
+        add_theme_support('post-formats', array( 'aside', 'gallery', 'quote', 'image', 'video' ));
     }
 }
-add_action('after_theme_setup', 'weavrstudio_setup');
 
+function add_mytheme_scripts()
+{
+    wp_enqueue_style('style', get_stylesheet_uri());
+}
+
+add_action('after_theme_setup', 'weavrstudio_setup', 0);
+add_action('wp_enqueue_scripts', 'add_mytheme_scripts');
--- wp-content/themes/weavrstudio/page.php
+++ wp-content/themes/weavrstudio/page.php
@@ -1,5 +1,8 @@
 <?php
+
 get_header();
+wp_nav_menu();
 if(have_posts()) : the_post();
     the_content();
 endif;
+get_footer();
